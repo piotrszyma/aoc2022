b611f0348ddf96192c35e4040684a6090f90ce8f
Piotr Szyma <pszyma@opera.com>
2022-12-03 12:18:41 +0100

    fix: part2

--- day3.part2.py
+++ day3.part2.py
@@ -1,3 +1,5 @@
+import functools
+import itertools
 from typing import Iterable
 
 
@@ -10,27 +12,35 @@ def get_priority(symbol: str) -> int:
         return ord(symbol) - 38
 
 
-def triplets(iterable: Iterable[str]) -> Iterable[list[str]]:
-    triplet = []
+def triplets(iterable: Iterable[str]) -> Iterable[tuple[str, str, str]]:
+    triplet = tuple()
     for item in iterable:
-        triplet.append(item.strip())
+        triplet = (*triplet, item.strip())
 
         if len(triplet) == 3:
             yield triplet
-            triplet = []
+            triplet = tuple()
 
 
 def main():
     with open("day3.input.txt", "r") as f:
         priority_total = 0
 
-        for (first, second, third) in triplets(f):
-            first_uniq = set(first)
-            second_uniq = set(second)
-            third_uniq = set(third)
-            [common_item] = first_uniq & second_uniq & third_uniq
+        for groups in triplets(f):
 
-            priority_total += get_priority(common_item)
+            # This one is less readable.
+            # [badge] = functools.reduce(
+            #     lambda prev_uniq, item: set(prev_uniq) & set(item),
+            #     groups[1:],  # Apply all other groups.
+            #     set[str](groups[0]),  # Start with set over first group.
+            # )
+
+            unique = set(groups[0])
+            for group in groups[1:]:
+                unique &= set(group)
+            [badge] = unique
+
+            priority_total += get_priority(badge)
 
         print(priority_total)
 

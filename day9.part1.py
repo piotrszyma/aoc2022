9dd3cf5dbfe1ee758b287201d6a721f43e3fb156
Piotr Szyma <pszyma@opera.com>
2022-12-09 17:45:39 +0100

    feat: day9 part 1

--- day9.part1.py
+++ day9.part1.py
@@ -1,33 +1,4 @@
 # Day 9
-
-
-# H
-# T
-
-#  0 1 2 <-- col_id
-# 0
-# 1
-# 2
-# /\
-# row_id
-
-#
-# H
-# T
-#
-# H (-1, 0) --> T (-1, 0)
-# T (_, _) --> T (0, 0)
-#
-
-#
-#  H
-# T
-#
-# H (-1, 0) --> T(-1, 1)
-# H (0, 1) --> T()
-
-# if is_adjacent(new_head_pos,
-import collections
 from io import TextIOWrapper
 from typing import Iterable, Literal, cast
 
@@ -37,7 +8,8 @@ Move = Literal["U", "D", "L", "R"]
 def is_adjancent(p1: tuple[int, int], p2: tuple[int, int]) -> bool:
     (x1, y1), (x2, y2) = p1, p2
     return (
-        (x1 == x2 and abs(y1 - y2) == 1)
+        (x1 == x2 and y1 == y2)
+        or (x1 == x2 and abs(y1 - y2) == 1)
         or (y1 == y2 and abs(x1 - x2) == 1)  # same row
         or (abs(x1 - x2) == abs(y1 - y2) == 1)  # same col  # diagonally adjacent
     )
@@ -50,12 +22,10 @@ def apply_move(
     (row, col) = pos
     match move:
         case "U":
-            assert row > 0
             return (row - 1, col)
         case "D":
             return (row + 1, col)
         case "L":
-            assert col > 0
             return (row, col - 1)
         case "R":
             return (row, col + 1)
@@ -63,11 +33,11 @@ def apply_move(
     raise ValueError(f"unexpected move {move}")
 
 
-def consume_moves(moves: Iterable[Move]):
+def consume_moves(moves: Iterable[Move]) -> set[tuple[int, int]]:
     H = (0, 0)
     T = (0, 0)
-    visited_by_tail = collections.Counter()
-    visited_by_tail[T] = 1
+    visited_by_tail = set()
+    visited_by_tail.add(T)
     for move in moves:
         old_H = H
         old_T = T
@@ -78,10 +48,7 @@ def consume_moves(moves: Iterable[Move]):
         else:
             new_T = old_H
 
-        x, y = new_T
-        assert x >= 0
-        assert y >= 0
-        visited_by_tail[new_T] += 1
+        visited_by_tail.add(new_T)
         T = new_T
         H = new_H
 
@@ -98,10 +65,10 @@ def moves_from_lines(file: TextIOWrapper) -> Iterable[Move]:
 
 
 def main():
-    with open("day9.input.test.txt", "r") as f:
+    with open("day9.input.txt", "r") as f:
         visited_by_tail = consume_moves(moves_from_lines(f))
-        breakpoint()
-        breakpoint()
+
+    print(len(visited_by_tail))
 
 
 if __name__ == "__main__":

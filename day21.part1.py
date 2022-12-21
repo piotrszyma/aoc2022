62c20bb201c4e929f929b79ff7247dfc452e1e10
Piotr Szyma <pszyma@opera.com>
2022-12-21 19:24:54 +0100

    feat: day21 part 1

---
+++ day21.part1.py
@@ -0,0 +1,94 @@
+from dataclasses import dataclass
+import operator
+
+# Day 21
+
+
+from typing import Callable, Literal, TypeAlias, Union
+
+
+class CalcExpr:
+    def calc(self) -> int:
+        ...
+
+
+class ValueExpr(CalcExpr):
+    def __init__(self, value: int):
+        self.value = value
+
+    def calc(self) -> int:
+        return self.value
+
+
+class MultExpr(CalcExpr):
+    def __init__(self, left: CalcExpr, right: CalcExpr):
+        self.left = left
+        self.right = right
+
+    def calc(self) -> int:
+        return self.left.calc() * self.right.calc()
+
+
+VarName: TypeAlias = str
+
+
+@dataclass
+class CalcExpr:
+    op: Callable[[int, int], int]
+    vars: tuple[VarName, VarName]  # Name of arguments.
+    kind: Literal["calc"] = "calc"
+
+
+@dataclass
+class ValueExpr:
+    value: int
+    kind: Literal["value"] = "value"
+
+
+ExprTable = dict[VarName, Union[CalcExpr, ValueExpr]]
+
+
+def calc(expr_table: ExprTable, variable: VarName) -> int:
+    expr = expr_table.get(variable)
+    if expr is None:
+        raise ValueError(f"Unexpected variable {variable}")
+
+    if expr.kind == "value":
+        return expr.value
+
+    arg0, arg1 = (calc(expr_table, variable) for variable in expr.vars)
+
+    return expr.op(arg0, arg1)
+
+
+def main():
+    expr_table: dict[str, Union[CalcExpr, ValueExpr]] = {}
+
+    with open("day21.input.txt", "r") as f:
+        for line in f:
+            target_variable, raw_expr = line.strip().split(": ")
+            if raw_expr.isdigit():
+                expr_table[target_variable] = ValueExpr(value=int(raw_expr))
+                continue
+
+            left, raw_op, right = raw_expr.split(" ")
+
+            match raw_op:
+                case "+":
+                    op = operator.add
+                case "-":
+                    op = operator.sub
+                case "*":
+                    op = operator.mul
+                case "/":
+                    op = operator.truediv
+                case _:
+                    raise ValueError(f"Unexpected operator symbol {raw_op}")
+
+            expr_table[target_variable] = CalcExpr(op, (left, right))
+
+    print(calc(expr_table, "root"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day21.part2.py
@@ -0,0 +1,8 @@
+# Day 21
+
+def main():
+    with open("day21.input.test.txt", "r") as f:
+        ...
+
+if __name__ == "__main__":
+    main()
